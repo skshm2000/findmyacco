3dae5e34d546f26b826537160a18352219e89c70
Dante2109 <akshayverma4299@gmail.com>
2023-02-16 13:41:27 +0530

    Homepage initialized

---
+++ findmyacco/components/HomepageComponents/CitiesGrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CitiesGrid = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default CitiesGrid;
---
+++ findmyacco/components/HomepageComponents/IdealGrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IdealGrid = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default IdealGrid;
