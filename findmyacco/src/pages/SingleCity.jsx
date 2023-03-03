a3ab4ac79b1f676bbef8a8cc2fcf78b497cb6f45
Saksham <saksham@everstage.com>
2023-03-03 14:27:56 +0530

    Last from here

--- findmyacco/src/pages/SingleCity.jsx
+++ findmyacco/src/pages/SingleCity.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useNavigate, useParams } from 'react-router-dom'
-import { Heading, Spinner } from '@chakra-ui/react'
+import { useLocation, useNavigate, useParams, useSearchParams } from 'react-router-dom'
+import { Heading, Spinner, Stack, Flex, Image, Text } from '@chakra-ui/react'
 
 const API = import.meta.env.VITE_GETLANDLORDS_API
 export const SingleCity = () => {
   const [ data, setData ] = useState([])
   let { city, country } = useParams()
+  const [ searchParams ] = useSearchParams()
   const nav = useNavigate()
 
   async function dataGetter() {
@@ -21,12 +22,37 @@ export const SingleCity = () => {
 
   return (
     <>
+    <Heading
+    textAlign={'center'}
+    >Choose Accomodation by area</Heading>
     { data.length==0 && <Spinner /> }
-    { data.length && <>
-      {data.map(ele=><Heading onClick={()=>{
-        nav(`/${country}/${city}?`)
-      }}>{ele.title}</Heading>)}
-    </> }
+    { data.length 
+    && 
+    <Flex
+    justifyContent={'space-around'}
+    w='80%'
+    m='auto'
+    >
+      {data.map(ele=>
+      <Stack
+      position={'relative'}
+      border='1px solid red'
+      w='25%'
+      gap={'20px'}
+      h='250px'
+      borderRadius={'20px'}
+      p='10px'
+      onClick={()=>{
+        nav(`/properties?landlord=${ele.landlord}`)
+      }}
+      >
+        <Text onClick={()=>{
+          nav(`/${country}/${city}?`)
+        }}>{ele.title}</Text>
+      </Stack>
+      )}
+    </Flex> 
+    }
     </>
   )
 }
