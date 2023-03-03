7e60719095cc4230dd037ce1da0d10108dffbabb
skshm2000 <107468465+skshm2000@users.noreply.github.com>
2023-03-03 14:57:53 +0530

    Strurents backend complete

--- backend/src/Routes/GetAcco.route.js
+++ backend/src/Routes/GetAcco.route.js
@@ -5,7 +5,6 @@ const GetAcco = Router()
 
 GetAcco.get('/', async (req, res)=>{
     const { landlord } = req.query
-    let query = req.query
     
     let data = await GetAccoByLandlord(landlord)
 
--- findmyacco/src/pages/Properties.jsx
+++ findmyacco/src/pages/Properties.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import axios from 'axios'
 
+const API = import.meta.env.VITE_GETACCOS_API
 export const Properties = () => {
+  let [ searchParams ] = useSearchParams()
+  let [ properties, setProperties ] = useState([])
+  async function dataGetter() {
+    let res = await axios.get(`${API}?landlord=${searchParams.get("landlord")}`)
+    let data = await res.data
+    
+    setProperties(data.data.properties)
+  }
+
+  console.log(properties)
+
+  useEffect(()=>{
+    dataGetter()
+  }, [])
   return (
     <>
     </>
